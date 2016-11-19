6fdce09f454cb0ce1f85b74957ebd7b191cb4b55
NoJsJa <yangwei13@outlook.com>
2016-11-19 20:04:43 +0800

    working in chat module.

--- models/CourseBroadcast.js
+++ models/CourseBroadcast.js
@@ -6,6 +6,10 @@
 
 // 解码中文url
 var url = require('url');
+// 定位文件
+var locateFromRoot = require('./tools/LocateFromRoot');
+// 文件操作模块
+var fs = require('fs');
 
 // 课程直播模块
 function courseBroadcast(io){
@@ -16,12 +20,15 @@ function courseBroadcast(io){
     // 客户端新建连接
     io.on('connection', function (socket) {
 
+        // 该客户端上传的文件
+        var Files = {};
         console.log('new socket connection coming...');
         // 获取当前用户连接的url进一步获取url房间号
         var url = socket.request.headers.referer;
         console.log('req headers: '+ JSON.stringify(socket.request.headers));
         // 分割地址字符串,数组的最后一个设置成房间号
         var split_arr = url.split('/');
+
         var roomId = split_arr[split_arr.length - 1];
         // 解码中文字符
         roomId = decodeURIComponent(roomId);
@@ -29,9 +36,22 @@ function courseBroadcast(io){
 
         // 用户姓名
         var user = '';
+
         // 监听连接的其它信息
         // 加入房间
-        socket.on('join', function (userName) {
+        socket.on('join', join);
+        // 监听来自客户端的消息
+        socket.on('message', message);
+        // 文件开始上传信号
+         socket.on('start', start);
+        // 文件上传中
+        socket.on('upload', upload);
+        // 关闭连接
+        socket.on('disconnect', disconnect);
+
+
+        // 加入房间
+        function join(userName) {
 
             console.log('join');
             user = userName;
@@ -46,10 +66,10 @@ function courseBroadcast(io){
             // 触发系统消息
             socket.to(roomId).emit('systemMessage', user + '加入了房间!');
             socket.emit('systemMessage', user + '加入了房间!');
-        });
+        };
 
-        // 监听来自客户端的消息
-        socket.on('message', function (msg) {
+        // 有新消息
+        function message(msg) {
 
             console.log('message');
             // 验证如果用户不在房间则不发消息
@@ -59,27 +79,147 @@ function courseBroadcast(io){
             // 发送给执行用户
             socket.to(roomId).emit('newMessage', msg, user);
             socket.emit('newMessage', msg, user);
-        });
+        }
+        
+        // 文件开始上传信号
+        function start(info) {
 
-        // 关闭连接
-        socket.on('disconnect', function () {
+            console.log('start...');
+            // 文件名和文件大小和文件类型,课程名字
+            var name  = info.Name;
+            var size = info.Size;
+            var courseName = info.CourseName;
+            var type = "";
+
+            //正则匹配判断文件类型
+            var regVideo = /(mp4|flv|rmvb|wmv|mkv)/i;
+            var regAudio = /(mp3|ape|wav|ogg|wma)/i;
+            var regImage = /(bmp|jpg|jpeg|svg|png|gif)/i;
+            // 类型结尾
+            if(regAudio.test( name.split('.').pop()) ){
+                type = "audios";
+            }else if(regImage.test( name.split('.').pop() )){
+                type = "images";
+            }else if(regVideo.test( name.split('.').pop() )){
+                type = "videos";
+            };
+            // 验证文件的存放地址
+            var prePath = locateFromRoot( ['/public/', type, '/courses/', courseName].join('') );
+            // 不存在则创建目录
+            if(!fs.existsSync(prePath)){
+                console.log('build dir.');
+                fs.mkdirSync(prePath);
+            };
+            // 完整地址
+            var filePath = locateFromRoot( ['/public/', type, '/courses/',
+                courseName, '/', name].join('') );
+            console.log('filePath:'　+ filePath);
+            // 统一存放文件的信息, 单位字节
+            // 文件大小,data数据缓冲区(最大10M),downloaded已上传的长度,handler文件描述符
+            Files[name] = {
+                fileSize: size,
+                data: '',
+                downloaded: 0,
+                handler: null,
+                filePath: filePath
+            };
+
+            // 打开磁盘文件用于上传准备工作
+            fs.open(Files[name].filePath, 'a', 0755, function (err, fd) {
+
+                if (err){
+                    console.log('[start] file open error: ' + err.toString());
+                }else {
+                    Files[name].handler = fd;
+
+                    console.log('ready to get data.');
+
+                    // 触发客户端上传数据
+                    socket.emit('moreData', {
+                        position: 0,
+                        percent: 0
+                    });
+                }
+
+            });
+
+            // 获取上传百分比
+            Files[name].getPercent = function () {
+
+                return parseInt( (this.downloaded / this.fileSize) * 100 );
+            };
+
+            // 获取文件已经上传的长度 -- 用于客户端截取文件上传, 单位字节
+            Files[name].getPosition = function () {
+
+                return this.downloaded;
+            };
+
+        }
+
+        // 文件上传中
+        function upload(info) {
+
+            console.log('uploading...');
+            // 名字和分段数据
+            var name = info.Name;
+            var segment = info.Segment;
+
+            // 已经上传的长度
+            Files[name].downloaded += segment.length;
+            // 组合数组(最大10M的缓存)
+            Files[name].data += segment;
+
+            // 上传完毕
+            if(Files[name].downloaded == Files[name].fileSize){
+
+                console.log('upload done.');
+                fs.write(Files[name].handler, Files[name].data, null, 'binary',function (err, written) {
+                    // 删除缓存文件
+                    delete Files[name];
+                    socket.emit('done');
+                });
+              // 10M的buffer被使用完
+            }else if(Files[name].data.length > 1024 * 1024 * 10){
+
+                console.log('file splice.');
+                fs.write(Files[name].handler, Files[name].data, null, 'binary',function (err, written) {
+                    Files[name].data = '';
+                    socket.emit('moreData', {
+                        'position': Files[name].getPosition(),
+                        'percent': Files[name].getPercent()
+                    });
+                });
+            //    继续填充缓冲区
+            }else {
+
+                console.log('more data');
+                socket.emit('moreData', {
+                    'position': Files[name].getPosition(),
+                    'percent': Files[name].getPercent()
+                });
+            }
+        }
+
+        // 客户端连接关闭
+        function disconnect() {
 
             console.log('disconnect');
             // 离开房间
             socket.leave(roomId, function (err) {
-               if(err){
-                   console.log('leave room error!');
-               }else {
-                   var index = roomUser[roomId].indexOf(user);
-                   if(index !== -1){
-                       // 数组删除
-                       roomUser[roomId].splice(index, 1);
-                       // 在房间内发布离开的消息
-                       socket.to(roomId).emit('systemMessage', user + '离开了房间!');
-                   }
-               }
+                if(err){
+                    console.log('leave room error!');
+                }else {
+                    var index = roomUser[roomId].indexOf(user);
+                    if(index !== -1){
+                        // 数组删除
+                        roomUser[roomId].splice(index, 1);
+                        // 在房间内发布离开的消息
+                        socket.to(roomId).emit('systemMessage', user + '离开了房间!');
+                    }
+                }
             });
-        });
+        }
     });
 }
 
--- public/javascripts/course_broadcast.js
+++ public/javascripts/course_broadcast.js
@@ -19,19 +19,20 @@ $(function () {
 
 /**
  * 页面变量
+ * courseName -- 直播间的名字(课程名字)
  * message -- 页面消息对象,
  * 包含收到的消息(received)和即将发送(send)的消息.
  * value -- 接收/发送的消息对象, watcherList -- 存储所有观察者
  * listen -- 注入新的观察者, trigger -- 触发所有观察者
  * socket -- 页面初始化时连接到服务器的socket对象
- * file -- 文件对象, 包括图片,音频和视频对象
- *
+ * file -- origin 未转换前的文件, data -- 转换后即将上传的文件, type -- 文件类型
+ * reader -- 文件读取对象fileReader
  * */
 
 var broadcastAction = {
 
     socket: {},
-
+    courseName: "",
     message: {
         send: {
             value: {},
@@ -47,9 +48,9 @@ var broadcastAction = {
         }
     },
     file: {
-        video: {},
-        audio: {},
-        image: {}
+        reader: {},
+        origin: "",
+        type: ""
     }
 };
 
@@ -97,10 +98,31 @@ broadcastAction.socketInit = function () {
 
     });
 
+    // 服务器请求上传更多数据
+    broadcastAction.socket.on('moreData', function (data) {
+
+        // 触发上传
+        broadcastAction.message.received.trigger('moreData', {
+            percent: data.percent,
+            position: data.position
+        });
+
+    });
+
+    // 服务器文件上传完成
+    broadcastAction.socket.on('uploadDone', function (data) {
+
+        broadcastAction.file.reader = null;
+        broadcastAction.file.origin = null;
+    });
+
 };
 
 /* 页面事件绑定 */
 broadcastAction.pageEventBind = function () {
+
+    // 当前课程名(也是直播间的名字)
+    broadcastAction.courseName = $('.broadcast-room').text().trim();
 
     // 用户发送消息
     $('#messageSend').click(function () {
@@ -119,7 +141,7 @@ broadcastAction.pageEventBind = function () {
     // 文件选中上传事件
     $('#fileChoose').on('change', function () {
 
-
+        console.log('on change');
         //判断浏览器是否支持FileReader接口
         if(typeof FileReader == 'undefined'){
             //使选择控件不可操作
@@ -129,9 +151,28 @@ broadcastAction.pageEventBind = function () {
 
         // 获取文件
         var file = document.getElementById('fileChoose').files[0];
-        broadcastAction.file.image = file;
-        if(file){
 
+        if(file){
+            broadcastAction.file.origin = file;
+            broadcastAction.file.reader = new FileReader();
+            // 注意这儿载入的是文件分片后的数据
+            // 这个需要服务器返回第一次返回后客户端确认信息
+            broadcastAction.file.reader.onload = function (event) {
+                console.log('reader onload.');
+                /* 通过内部的result对象取到读取后的数据 */
+                broadcastAction.socket.emit('upload', {
+                    "Name": broadcastAction.file.origin.name,
+                    "Segment": event.target.result
+                })
+            };
+            console.log('start upload');
+            // 触发开始上传事件
+            // 等待服务器发回允许上传的回调信息后开始载入文件流式传输到服务器
+            broadcastAction.socket.emit('start', {
+               "Name": broadcastAction.file.origin.name,
+                "Size": broadcastAction.file.size,
+                "CourseName": broadcastAction.courseName
+            });
         }
     });
     
@@ -222,6 +263,50 @@ broadcastAction.watcherActive = function () {
     broadcastAction.message.received.listen('systemMessage', receiveSystemMsg);
     // 接收用户消息
     broadcastAction.message.received.listen('newMessage', receiveNewMsg);
+    // 接收上传开始上传文件的信息
+    broadcastAction.message.received.listen('moreData', startUpload);
+
+    // 开始上传数据
+    function startUpload(info) {
+
+        console.log('uploading...');
+        // 每次上传的长度512kb, 524288字节
+        var length = 524288;
+        //上传的位置和进度
+        // position相除减小过数字
+        var position = info.position * length;
+        var percent = info.percent;
+
+        // 分割的文件对象
+        var newFile = null;
+        // 目前的操作对象
+        var currentFile = broadcastAction.file.origin;
+
+        // 更新页面进度条
+        $('.progress-bar').css('width', percent + '%');
+        $('.progress-bar > span').text(percent + '%完成');
+
+        // 裁切文件并兼容浏览器
+        if(currentFile.slice){
+            newFile = currentFile.slice(position, position +
+                Math.min(length, currentFile.size - position));
+
+        }else if(broadcastAction.file.origin.webkitSlice){
+            newFile = currentFile.webkitSlice(position, position +
+                Math.min(length, currentFile.size - position));
+
+        }else if(broadcastAction.file.origin.mozSlice){
+            newFile = currentFile.mozSlice(position, position +
+                Math.min(length, currentFile.size - position));
+
+        }
+        // 触发文件上传二进制
+        if(newFile){
+            broadcastAction.file.reader.readAsBinaryString(newFile);
+        }
+
+
+    }
 
     // 接收系统消息
     function receiveSystemMsg(info) {
